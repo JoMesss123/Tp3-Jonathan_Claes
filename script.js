dc4e325728be84622eb108971185306e7c64ad1e
Jonathan Claes <115749108+JoMesss123@users.noreply.github.com>
2023-08-28 20:30:53 -0400

    21

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
 // MENU MOBILE
-const btn = document.getElementById('menu_btn');
-const nav = document.getElementById('menu');
-const barsIcon = document.querySelector('#menu_btn svg:first-child');
-const timesIcon = document.querySelector('#menu_btn svg:last-child');
-
-btn.addEventListener('click', () => {
-    btn.classList.toggle('open');
-    nav.classList.toggle('hidden');
-    document.body.classList.toggle('overflow-hidden');
-    toggleMenuIcon();
-});
-
-function toggleMenuIcon() {
-    barsIcon.classList.toggle('hidden');
-    timesIcon.classList.toggle('hidden');
-}
 
 
 // INFOLETTRE - VALIDATION DU FORMULAIRE
